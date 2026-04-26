69dc22841c6f5c791463adb6f72d61569d78acb5
WorkaholicCat <aartha.tab@gmail.com>
2026-04-26 21:30:03 +0530

    About card frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Shape from "./assets/SVG/Asset10.svg?react";
+import Star from "./assets/SVG/Background/Asset8.svg?react";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function AboutCard() {
     <div className="about-card">
       <div className="pic"></div>
       <Shape className="blob" />
+      <Star className="star" />
       <div className="contact">
         <h3>Contacts</h3>
         <ul>
@@ -24,6 +26,7 @@ function AboutCard() {
           <li>LinkedIn:</li>
         </ul>
       </div>
+
       <h2> About Me </h2>
       <p> Frontend-focused Web Developer with strong skills in HTML, CSS, and JavaScript, currently expanding into MERN stack. Experienced in Python, MySQL, R and data visualization, with the ability to build user-friendly interfaces and analyze data effectively. Passionate about creating responsive web applications and intuitive UI/UX designs. </p>
     </div>
