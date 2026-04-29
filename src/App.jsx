7a97a839d2e315083939266d29e26070f6e7ee60
WorkaholicCat <aartha.tab@gmail.com>
2026-04-29 11:08:36 +0530

    Completing custom grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import Shape from "./assets/SVG/Asset10.svg?react";
 import Star from "./assets/SVG/Background/Asset8.svg?react";
+import Techstack from "./assets/SVG/Techstack/base.svg?react";
+import Tool from "./assets/SVG/Tools/blob2.svg?react";
 
 function App() {
   return (
@@ -26,9 +28,12 @@ function AboutCard() {
           <li>LinkedIn:</li>
         </ul>
       </div>
-
       <h2> About Me </h2>
       <p> Frontend-focused Web Developer with strong skills in HTML, CSS, and JavaScript, currently expanding into MERN stack. Experienced in Python, MySQL, R and data visualization, with the ability to build user-friendly interfaces and analyze data effectively. Passionate about creating responsive web applications and intuitive UI/UX designs. </p>
+      <Techstack className="base" />
+      <h3 className= 'Techstack'>Tech Stack</h3>
+      <Tool className="blob2" />
+      <h3 className= 'Tools'>Tools</h3>
     </div>
   )
 }
